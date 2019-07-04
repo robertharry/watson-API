89ccd1a63d4da6de864d6519d83c63e3a0046c15
robertharry <47328927+robertharry@users.noreply.github.com>
2019-07-04 08:19:07 +0100

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-const iam_apikey = 'LjubKfzb2n2xe2BxxGR89hraMYK0d61CWe6UYppdMUk8'
-const watsonUrl = 'https://gateway-lon.watsonplatform.net/tone-analyzer/api'
-
-
-module.exports = {iam_apikey, watsonUrl}
